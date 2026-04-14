b441cead6c40c43efef466deb34dbe39e5ffcd02
jesusjoseg <jesusjosegardea@gmail.com>
2026-04-14 11:03:43 -0600

    la Apertura

--- Apertura.py
+++ Apertura.py
@@ -1,7 +1,5 @@
-from PyQt5 import QtWidgets,QtCore,QtGui
-from PyQt5.QtWidgets import QMainWindow
-from datashader import layout
-
+from PyQt5 import QtCore,QtGui
+from PyQt5.QtWidgets import (QMainWindow,QVBoxLayout,QLabel,QLineEdit,QComboBox,QPushButton)
 
 class Apertura(QMainWindow):
     def __init__(self):
@@ -9,15 +7,14 @@ class Apertura(QMainWindow):
         self.setWindowTitle("Configuración Inicial - Kitsune")
         self.setFixedSize(400,500)
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-        layout = QtWidgets.QVBoxLayout()
+        layout = QVBoxLayout()
 
-        self.label = QtWidgets.QLabel("<h2>¡Bienvenido a Kitsune!</h2>")
-        self.Txt_nombre=QtWidgets.QLineEdit()
+        self.label = QLabel("<h2>¡Bienvenido a Kitsune!</h2>")
+        self.Txt_nombre=QLineEdit()
         self.Txt_nombre.setPlaceholderText("Nombre De Tu Empresa")
         layout.addWidget(self.Txt_nombre)
-        layout.addWidget(QtWidgets.QLabel("El giro de Negocio:"))
-        self.cbx_rubro = QtWidgets.QComboBox()
+        layout.addWidget(QLabel("El giro de Negocio:"))
+        self.cbx_rubro = QComboBox()
         self.cbx_rubro.addItems(["Abarrotes","Boutique","Farmacia","Ferreteria","Otro"])
         layout.addWidget(self.cbx_rubro)
         layout.addSpacing(20)
-        layout
